c4048267fd483b10f86491435f57f6f2c159e779
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-11-21 20:38:10 +0200

    add the header design

---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,32 @@
+import searchIcon from "../../assets/icon-search.svg";
+import heartIcon from "../../assets/icon-heart.svg";
+import cartIcon from "../../assets/icon-cart.svg";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div className="border-b border-[#0000004d] py-6 flex items-center justify-center">
+      <div className="container flex justify-between items-center">
+        <div className="font-[Inter] font-bold text-2xl">Exclusive</div>
+        <ul className="flex gap-12">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+        </ul>
+        <div className="flex items-center gap-6">
+          <div className="relative">
+            <input type="text" placeholder="What are you looking for?" className="bg-[#F5F5F5] px-4 py-2 text-sm max-w-65" />
+            <img src={searchIcon} className="absolute top-[50%] right-4 translate-y-[-50%]" />
+          </div>
+          <button>
+            <img src={heartIcon} />
+          </button>
+          <button>
+            <img src={cartIcon} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
